6976d21947b60caadded9af6e82d0cfddfe9cd12
MichaLL27 <lazarashvili2704@gmail.com>
2024-10-03 06:17:47 +0400

    create userProfileEdit component and create router for it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserProfileEditComponent } from './user-profile-edit/user-profile-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'edit-profile', component: UserProfileEditComponent },
+];
